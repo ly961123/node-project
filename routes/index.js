8a31ad539b8522d21922a832a223d1e21a4c1467
ly <905266021@qq.com>
2018-11-23 21:06:48 +0800

    简单的修改了用户的表格信息

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/user.html', function (req, res) {
+  res.render('user');
+})
+router.get('/phone.html', function (req, res) {
+  res.render('phone');
+})
+router.get('/brand.html', function (req, res) {
+  res.render('brand');
+})
 
 module.exports = router;
